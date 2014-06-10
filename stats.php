a54367c4b72855611a4721cc0122b65f4db3c93e
nekloth <nekloth@gmail.com>
2014-06-10 14:11:03 +0200

    [STATS] Consommation annuelle
    ... commencée, mais non terminée.

--- stats.php
+++ stats.php
@@ -7,48 +7,100 @@
 	
 	
 	if ($db = new sqlite3($dbname)) {
-		echo "On commence.<br/>";
 
+	?>
 
+		<h3>Consommation annuelle moyenne</h3>
+		<div class="row">
+			<div class="col-md-6">
+				<?php
+				
+				$results = @$db->query($REQ_conso_moyenne_annuelle);
+				$num_item = 0;
+				$cumul_annuel = 0;
+				$precedente_mesure = 0;
+				$nombre_jours = 0;
+				$annee_en_cours = 0;
+				$position = 0;
 
-		
-		
-		//Calcul du total des dépenses, par Année
-		$results = @$db->query($REQ_depense_par_annee);
+				while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
+					$position++;
+					
+					echo "Analyse de $position (". $row['date'] .") : $precedente_mesure (avant), " . $row['quantite'] . " (maintenant)";
+					
+					if ($position == 1) {
+						$num_item++;
+						$annee_en_cours = $row['annee'];
+						$precedente_mesure = $row['quantite'];
+						echo " - INIT <br/>";
+					} else {
+					
+
+						if ($precedente_mesure > $row['quantite']) { //On prendre en compte, il ne s'agit pas d'un plein
+							echo " - On prend en compte <br/>";
+							$num_item++;
+							$cumul_annuel += $precedente_mesure - $row['quantite']; 
+							$precedente_mesure = $row['quantite'];
+							$nombre_jours = 0;
+						} else {
+							$precedente_mesure = $row['quantite'];
+							echo " - On NE prend PAS en compte <br/>";
+						}
 	
+						if ($row['annee'] != $annee_en_cours) {
+							echo "     -> Bilan annee $annee_en_cours : $num_item mesures, jours : $nombre_jours, quantite = $cumul_annuel<br/>";
+							$annee_en_cours = $row['annee'];
+							$num_item = 0;
+							$cumul_annuel = 0;
+							$nombre_jours = 0;
+							
+						} 
 
+
+					}
+
+					
+					//	var_dump($row);
+					
+					
+				}
+				echo "     -> Bilan annee $annee_en_cours : $num_item mesures, jours : $nombre_jours, quantite = $cumul_annuel<br/>";
+				?>
+				
+				
+			</div>
+			<div class="col-md-6">
+				Titi
+			</div>
 		
-		?>
+		
+		</div>
+
+
+
 		<h3>Dépenses annuelles</h3>
 		<div class="row">
 
 			<div class="col-md-6">
 				<table class='table table-striped table-bordered table-hover table-condensed'>
-				<tr>
-					<th>Année</th>
-					<th>Coût</th>
-					<th>Quantité</th>
-				</tr>
-
-			
-
-
-				<?php
-				
-				$num_item = 0;
-				$total_quantite = 0;
-				$total_prix = 0;
-				while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
-					//var_dump($row);
-					$num_item += 1;
-					$total_quantite += $row['quantite_achetee'];
-					$total_prix += $row['prix_total'];
-					echo "<tr><td>" . $row['annee'] . "</td><td>" . afficheReel($row['prix_total']) . " €</td><td>" . afficheEntier($row['quantite_achetee']) . " litres</td></tr>";
-					
-				}
-				
-				?>
-				
+					<tr>
+						<th>Année</th>
+						<th>Coût</th>
+						<th>Quantité</th>
+					</tr>
+					<?php
+					$results = @$db->query($REQ_depense_par_annee);
+					$num_item = 0;
+					$total_quantite = 0;
+					$total_prix = 0;
+					while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
+						//var_dump($row);
+						$num_item += 1;
+						$total_quantite += $row['quantite_achetee'];
+						$total_prix += $row['prix_total'];
+						echo "<tr><td>" . $row['annee'] . "</td><td>" . afficheReel($row['prix_total']) . " €</td><td>" . afficheEntier($row['quantite_achetee']) . " litres</td></tr>";
+					}
+					?>				
 				</table>
 			</div>
 			<div class="col-md-6">
