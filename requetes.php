1d21c28780716b7b27697cec4a2c3fd173669285
nekloth <nekloth@gmail.com>
2014-06-13 13:50:36 +0200

    (STATS) Conso, ça avance
    ... même si c'est pas encore ça (pas assez de mesures !!)

--- requetes.php
+++ requetes.php
@@ -2,6 +2,6 @@
 
 	$REQ_depense_par_annee = 'SELECT substr(date,0,5) AS annee, SUM(prix_total) as prix_total, SUM(quantite_achetee) as quantite_achetee FROM pleins GROUP BY annee ORDER BY annee desc;';
 
-	$REQ_conso_moyenne_annuelle = 'SELECT date,substr(date,0,5) AS annee, substr(date,5,2) AS mois, substr(date,7,2) AS jour, substr(date,9,2) AS heures, substr(date,11,2) as minutes, quantite FROM mesures ORDER BY date ASC;';
+	$REQ_conso_moyenne_annuelle = 'SELECT date,substr(date,0,5) AS annee, substr(date,5,2) AS mois, substr(date,7,2) AS jour, substr(date,9,2) AS heures, substr(date,11,2) as minutes, substr(date,13,2) as secondes, quantite FROM mesures ORDER BY date ASC;';
 	
 ?>
--- stats.php
+++ stats.php
@@ -15,40 +15,63 @@
 			<div class="col-md-6">
 				<?php
 				
+				
+				/*
+				$d1 = new DateTime('$row['annee']."-".$row['mois']."-".$row['jour']." ".$row['heures'].":".$row['minutes'].":".$row['secondes']'); 
+				$d2 = new DateTime('2009-06-10 20:30:00'); 
+				$diff = $d1->diff($d2); 
+				$nb_jours = $diff->d;
+				echo $nb_jours . "<br/><br/><br/><br/>";
+				*/
+				
+				
 				$results = @$db->query($REQ_conso_moyenne_annuelle);
 				$num_item = 0;
 				$cumul_annuel = 0;
 				$precedente_mesure = 0;
+				$precedente_date = new DateTime();
+				$date_consideree = new DateTime();
 				$nombre_jours = 0;
 				$annee_en_cours = 0;
 				$position = 0;
+				
 
 				while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
 					$position++;
 					
-					echo "Analyse de $position (". $row['date'] .") : $precedente_mesure (avant), " . $row['quantite'] . " (maintenant)";
+					echo "..$position..(". $row['date'] .") : de $precedente_mesure à " . $row['quantite'] . "";
 					
 					if ($position == 1) {
 						$num_item++;
 						$annee_en_cours = $row['annee'];
 						$precedente_mesure = $row['quantite'];
+						$precedente_date   =  new DateTime($row['annee']."-".$row['mois']."-".$row['jour']." ".$row['heures'].":".$row['minutes'].":".$row['secondes']);
+
 						echo " - INIT <br/>";
 					} else {
 					
 
 						if ($precedente_mesure > $row['quantite']) { //On prendre en compte, il ne s'agit pas d'un plein
-							echo " - On prend en compte <br/>";
+							$aCompter = $precedente_mesure - $row['quantite'];
 							$num_item++;
 							$cumul_annuel += $precedente_mesure - $row['quantite']; 
+							$date_consideree   =  new DateTime($row['annee']."-".$row['mois']."-".$row['jour']." ".$row['heures'].":".$row['minutes'].":".$row['secondes']);
+							$interval = $precedente_date->diff($date_consideree);
+							$nombre_jours_temp = $interval->days;
+							if ($interval->h>12) $nombre_jours_temp++;
+							$nombre_jours += $nombre_jours_temp;
+							echo " - Oui : ($aCompter litres et $nombre_jours_temp jours)<br/>";
 							$precedente_mesure = $row['quantite'];
-							$nombre_jours = 0;
+							$precedente_date   =  $date_consideree;
+
 						} else {
 							$precedente_mesure = $row['quantite'];
-							echo " - On NE prend PAS en compte <br/>";
+							$precedente_date   =  new DateTime($row['annee']."-".$row['mois']."-".$row['jour']." ".$row['heures'].":".$row['minutes'].":".$row['secondes']);
+							echo " - Non <br/>";
 						}
 	
 						if ($row['annee'] != $annee_en_cours) {
-							echo "     -> Bilan annee $annee_en_cours : $num_item mesures, jours : $nombre_jours, quantite = $cumul_annuel<br/>";
+							echo "     -> Bilan annee $annee_en_cours : $num_item mesures <br/> Jours : $nombre_jours<br/>Quantite = $cumul_annuel<br/>Conso annuelle: ". round(($cumul_annuel/$nombre_jours)*365.25,2) ."<br/><br/>";
 							$annee_en_cours = $row['annee'];
 							$num_item = 0;
 							$cumul_annuel = 0;
@@ -64,13 +87,14 @@
 					
 					
 				}
-				echo "     -> Bilan annee $annee_en_cours : $num_item mesures, jours : $nombre_jours, quantite = $cumul_annuel<br/>";
+							echo "     -> Bilan annee $annee_en_cours : $num_item mesures <br/> Jours : $nombre_jours<br/>Quantite = $cumul_annuel<br/>Conso annuelle: ". round(($cumul_annuel/$nombre_jours)*365.25,2) ."<br/><br/>";
 				?>
 				
 				
 			</div>
 			<div class="col-md-6">
-				Titi
+				<?php 
+				?>
 			</div>
 		
 		
